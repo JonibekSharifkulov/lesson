183c07a90e979532607cad9a8f75dfe988211e0b
Jonibek Sharifkulov <jonibeksharifkulov11@icloud.com>
2023-06-18 10:20:57 +0500

    homeTask

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
-let a = prompt("name");
+// let a = prompt("name");
 
-if (a.charAt() === "A" || a.charAt() === "a") {
-  let age = +prompt("enter age");
+// if (a.charAt() === "A" || a.charAt() === "a") {
+//   let age = +prompt("enter age");
 
-  if (age >= 20 && age <= 40) {
-    let balance = +prompt("enter your balance");
+//   if (age >= 20 && age <= 40) {
+//     let balance = +prompt("enter your balance");
 
-    if (balance >= 100) {
-      alert("welcome to club");
-    } else {
-      alert("you have not enough money");
-    }
-  } else {
-    alert("your age is not enough");
-  }
-} else {
-  alert("your name is not begining with a");
-}
+//     if (balance >= 100) {
+//       alert("welcome to club");
+//     } else {
+//       alert("you have not enough money");
+//     }
+//   } else {
+//     alert("your age is not enough");
+//   }
+// } else {
+//   alert("your name is not begining with a");
+// }
+
+// let arr = [12, 35, 56, 50, 33, 51, 88, 99, 23];
+// // let a = arr.map(Math.sqrt);
+
+// let oddnum = [];
+
+// let wholenum = [];
+
+// arr.map((i) => {
+//   if (i % 2 === 0) {
+//     oddnum.push(i);
+//   } else {
+//     wholenum.push(i);
+//   }
+// });
+
+// console.log(Math.max(oddnum.length, wholenum.length));
+
+// arr. danash bugi daroz nom
+// massiv nomerosh opshi  javobash
+//masii eng kalon nomer eng mayda nomer
+
+// for (let i = 0; i < arr.length; i++) {
+//   let max = arr[i];
+//   arr.map((i) => {
+//     if (i.length > max) {
+//       max = i;
+//     }
+//     console.log(max);
+//   });
+// }
+
+// arr.map((i) => {
+//   console.log(typeof i);
+// });
+
+//first
+// let arr = [12, 35, 56, 50, 33, 51, 88, 99, 23];
+// console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
+
+//second
+// let arr = ["joni", "azim", "jasurbek"];
+// let max = [""];
+// let search = arr.find((person) => person.length >= arr[person]);
+// console.log(search);
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (max < arr[i].length) {
+//     max = arr[i].length;
+//   }
+// }
+// console.log(max);
+
+//third
+// let arr = [12, 35, 56, 50, 33, 51, 88, 99, 23];
+
+// let res = arr.reduce((acc, rec) => acc + rec);
+// console.log(res);
